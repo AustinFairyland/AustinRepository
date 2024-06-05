40a6868539e425e117d0738393640855cf857924
Lionel Johnson(Work) <fariylandfuture@outlook.com>
2024-06-05 12:48:23 +0800

    :ambulance: Remove unused python scripts and refactor file locations
    Multiple unused Python scripts and files have been removed from the project. The remaining files have been reorganized and restructured to optimize the project's structure. Changes also include the update of .gitignore to exclude unnecessary files and directories.

--- projects/python2.7/demo-001/service/__init__.py
+++
@@ -1,8 +0,0 @@
-# coding: utf8
-""" 
-@software: PyCharm
-@author: Lionel Johnson
-@contact: https://fairy.host
-@organization: https://github.com/FairylandFuture
-@since: 2024-04-22 18:56:15 UTC+8
-"""
--- projects/python2.7/demo-001/test/__init__.py
+++
@@ -1,8 +0,0 @@
-# coding: utf8
-""" 
-@software: PyCharm
-@author: Lionel Johnson
-@contact: https://fairy.host
-@organization: https://github.com/FairylandFuture
-@since: 2024-04-23 10:00:22 UTC+8
-"""
