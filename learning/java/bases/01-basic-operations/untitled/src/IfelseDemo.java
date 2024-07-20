8ed284750c0e0107d305932a8918a8d4eb1ef7bd
Lionel Johnson(Work) <fariylandfuture@outlook.com>
2024-07-21 00:38:58 +0800

    :sparkles: Add Java demo code

---
+++ learning/java/bases/01-basic-operations/untitled/src/IfelseDemo.java
@@ -0,0 +1,19 @@
+/*****************************************************
+ * @software: IntelliJ IDEA
+ * @author: Lionel Johnson
+ * @contact: https://fairy.host
+ * @organization: https://github.com/FairylandFuture
+ * @since: @since: 2024-07-20 20:31:42 UTC+08:00
+ *****************************************************/
+
+import java.util.Random;
+
+/**
+ * @author Lionel Johnson
+ */
+public class IfelseDemo {
+    public static void main(String[] args) {
+        Random randomInt = new Random();
+        
+    }
+}
