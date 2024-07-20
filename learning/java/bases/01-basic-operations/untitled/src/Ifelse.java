a601bb92b09c2ad4bc70a8fe3f0ed51005912579
Lionel Johnson(Work) <fariylandfuture@outlook.com>
2024-07-20 19:57:29 +0800

    :sparkles: Add Java demo code

--- learning/java/bases/01-basic-operations/untitled/src/Ifelse.java
+++ learning/java/bases/01-basic-operations/untitled/src/Ifelse.java
@@ -17,5 +17,19 @@ public class Ifelse {
         } else {
             System.out.println("false");
         }
+        
+        int score = 70;
+        if (score >= 90) {
+            System.out.println("优秀");
+        } else if (score >= 70 && score < 90) {
+            System.out.println("良好");
+        } else if (score >= 60 && score < 70) {
+            System.out.println("一般");
+        } else {
+            System.out.println("较差");
+        }
+        
+        
+        
     }
 }
