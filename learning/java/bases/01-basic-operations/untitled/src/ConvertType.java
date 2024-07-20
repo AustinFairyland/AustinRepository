737ca11e20b2dedb0a805288a7b9c92af9587452
Lionel Johnson(ASUS-X556UR) <fairylandfuture@outlook.com>
2024-07-20 10:40:32 +0800

    :sparkles: Add java demo code

---
+++ learning/java/bases/01-basic-operations/untitled/src/ConvertType.java
@@ -0,0 +1,20 @@
+/*****************************************************
+ * @software: IntelliJ IDEA
+ * @author: Lionel Johnson
+ * @contact: https://fairy.host
+ * @organization: https://github.com/FairylandFuture
+ * @since: 2024-07-20 03:20:24 UTC+8
+ ******************************************************/
+
+/**
+ * @author Lionel Johnson
+ */
+public class ConvertType {
+    public static void main(String[] args) {
+        double scoreAvgFirst = 81.29;
+//        scoreAvgFirst += 2;
+        int add = 2;
+        double scoreAvgSec = scoreAvgFirst + add;
+        System.out.println("第二次的平均分: " + scoreAvgSec);
+    }
+}
