d7c1d98325ed0e4ed2b86f54efdae93a98c17bdf
Lionel Johnson(ASUS-X556UR) <fairylandfuture@outlook.com>
2024-07-21 13:26:06 +0800

    :sparkles: Add java demo code

--- learning/java/bases/01-basic-operations/untitled/src/IfelseDemo.java
+++ learning/java/bases/01-basic-operations/untitled/src/IfelseDemo.java
@@ -7,13 +7,39 @@
  *****************************************************/
 
 import java.util.Random;
+import java.util.Scanner;
 
 /**
  * @author Lionel Johnson
  */
 public class IfelseDemo {
     public static void main(String[] args) {
-        Random randomInt = new Random();
-        
+        Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
+
+        int randomInt = random.nextInt(0, 10);
+        System.out.println("幸运数字是: " + randomInt);
+
+        System.out.println("请输入会员编号");
+        if (scanner.hasNextInt()) {
+            int vipNumber = scanner.nextInt();
+            String vipStr = String.valueOf(vipNumber);
+
+            if (vipStr.length() == 4) {
+                char secChar = vipStr.charAt(1);
+                int sceValue = Character.getNumericValue(secChar);
+                if (sceValue == randomInt) {
+                    System.out.println("恭喜你中奖了...");
+                } else {
+                    System.out.println("再接再厉...");
+                }
+            } else {
+                System.out.println("请输入正确的会员编号.");
+            }
+        } else {
+            System.out.println("输入的会员编号无效.");
+        }
+
+        scanner.close();
     }
 }
