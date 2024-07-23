dde8fc05344f965397bb0f5ae7a5192f86a0302a
Lionel Johnson(ASUS-X556UR) <fairylandfuture@outlook.com>
2024-07-24 04:41:09 +0800

    :sparkles: Add python multiprocess demo code and java array demo code

--- learning/java/bases/01-basic-operations/untitled/src/JavaArrays.java
+++ learning/java/bases/01-basic-operations/untitled/src/JavaArrays.java
@@ -29,8 +29,26 @@ public class JavaArrays {
         System.out.println("=".repeat(50));
         System.out.println(Arrays.toString(args));
 
+        /*
+         * 数组属性: length -> 获取数组的长度
+         * */
         // 分配空间并初始化赋值
+        System.out.println("=".repeat(50));
         int[] ints1 = {1, 2, 3, 4, 5};
+        // length 数值长度属性
+        System.out.println("数组的长度: " + ints1.length);
+        for (int item : ints1) {
+            System.out.println(item);
+        }
+
+        /*
+         * Arrays中的sort和toString方法
+         *   sort: 对数组中的元素进行排序
+         *   toString: 转换为字符串
+         * */
+        int[] ints2 = {8, 4, 2, 1, 23, 344, 12};
+        Arrays.sort(ints2);
+        System.out.println(Arrays.toString(ints2));
 
 
     }
