838e277e6cded0e9547bc23cd029df8d664b0165
Lionel Johnson(ASUS-X556UR) <fairylandfuture@outlook.com>
2024-07-23 12:15:14 +0800

    :sparkles: Add python multiprocess demo code

--- learning/python/advanced/multiprocess/create_process_multiprocessing.py
+++ learning/python/advanced/multiprocess/create_process_multiprocessing.py
@@ -10,17 +10,35 @@
 import os
 import time
 import multiprocessing
+import threading
+from collections import deque
 
 
-def watch():
-    for i in range(3):
-        print("看电视....", os.getpid())
+def task(count: int, container: deque):
+    for i in range(count):
+        container.append(i)
         time.sleep(1)
 
 
-if __name__ == '__main__':
-    # 启动一个进程
-    print(os.getpid())
-    process = multiprocessing.Process(target=watch)
-    print(id(process), process)
-    process.start()
+if __name__ == "__main__":
+    print("单进程 单线程")
+    single_start_time = time.time()
+    data = deque()
+    task(2, data)
+    task(3, data)
+    print(data)
+    print(f"单进程 单线程 运行耗时: {time.time() - single_start_time}")
+
+    print("单进程 多线程")
+    multi_start_time = time.time()
+    data2 = deque()
+    t1 = threading.Thread(target=task, args=(2, data2))
+    t2 = threading.Thread(target=task, args=(3, data2))
+    t1                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   .daemon = True
+    t2.daemon = True
+    t1.start()
+    t2.start()
+    # t1.join()
+    # t2.join()
+    print(data2)
+    print(f"单进程 多线程 运行耗时: {time.time() - multi_start_time}")
