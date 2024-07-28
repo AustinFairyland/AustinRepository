edbd820947455f9a4b3d9af7bd29e77134168610
Lionel Johnson(Work) <fariylandfuture@protonmail.com>
2024-07-28 17:49:55 +0800

    :ambulance: Add new Java modules and Python tutorial
    Created new Java modules `01-t`, `test`, and their respective `Main.java` files. Added a comprehensive Python "Getting Started" tutorial notebook. Removed the old Java module `03-operator/03-operator.iml`.

---
+++ languages/java/1.beginner/basic-code/test/src/host/fairy/demo/Main.java
@@ -0,0 +1,17 @@
+/*****************************************************
+ * @software: IntelliJ IDEA
+ * @author: Lionel Johnson
+ * @contact: https://fairy.host
+ * @organization: https://github.com/FairylandFuture
+ * @since: @since: 2024-07-28 17:46:21 UTC+08:00
+ *****************************************************/
+package host.fairy.demo;
+
+/**
+ * @author Lionel Johnson
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
